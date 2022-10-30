2af88af1cb7c9c291440437bdf96612bf8645381
alex <format37@gmail.com>
2022-10-30 11:20:55 +0400

    new file:   download.sh

---
+++ download.sh
@@ -0,0 +1,5 @@
+# use wget to download the file from the url: https://lfs.aminer.cn/misc/cogvideo/cogvideo-stage1.zip
+# to the folder: models/
+wget https://lfs.aminer.cn/misc/cogvideo/cogvideo-stage1.zip -P models/
+# and file from url: https://lfs.aminer.cn/misc/cogvideo/cogvideo-stage2.zip
+wget https://lfs.aminer.cn/misc/cogvideo/cogvideo-stage2.zip -P models/
