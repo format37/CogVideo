3c12b4bc34140c099c9ff08285899497c6037d34
alex <format37@gmail.com>
2022-11-01 00:22:09 +0400

    new file:   copy.sh

---
+++ copy.sh
@@ -0,0 +1,6 @@
+mkdir ../sharefs/
+mkdir ../sharefs/cogview-new/
+# show progress bar and copy models/cogvideo-stage1.zip to ../sharefs/cogview-new/
+cp -v models/cogvideo-stage1.zip ../sharefs/cogview-new/
+# show progress bar and copy models/cogvideo-stage2.zip to ../sharefs/cogview-new/
+cp -v models/cogvideo-stage2.zip ../sharefs/cogview-new/
